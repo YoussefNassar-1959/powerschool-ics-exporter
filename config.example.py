21b4cd042f12c97331fa0b48e2c224b032fe2fef
Youssef Nassar <youssefnassar1961@gmail.com>
2025-09-26 12:14:04 +0200

    Create config.example.py

---
+++ config.example.py
@@ -0,0 +1,3 @@
+# Rename this file to `config.py` to customize
+POWER_SCHOOL_URL = "https://ala.powerschool.com/guardian/myschedule.html"
+TIMEZONE = "Africa/Johannesburg"  # African Leadership Academy is in South Africa
